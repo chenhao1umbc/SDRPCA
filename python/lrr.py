b1dee9852439db20492591ff6731ecbb8639fed4
Chen Hao <chenhao1@umbc.edu>
2019-08-05 11:42:39 -0400

    the python version of lrr is done, no obvious bug

--- python/lrr.py
+++ python/lrr.py
@@ -5,10 +5,10 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 data_sets = ['EYB', 'coil', 'AR']
 
 optdata={}
-optdata['data_set'] = data_sets[0]
+optdata['dataset'] = data_sets[0]
 optdata['ind_dataset'] = 1
 optdata['cv_fold'] = 5
-optdata['max_iter'] = 1000
+optdata['max_iter'] = 500
 optdata['rng'] = 0
 optdata['o_per'] = 0.1
 optdata['use_gpu'] = torch.cuda.is_available()
@@ -18,15 +18,28 @@ optdata['max_mu'] = 1e8
 
 
 # no outlier added just loading the data, in numpy format
-data0, labels0 = load_data()
-
-# split the data, normalized and corrupted, in torch.tensor format
-xtr, xtr_labels, xte, xte_labels = data_gen(data0, labels0, optdata)
-
-
-
-
+data, labels = load_data()
 knn = KNN(5)
-knn.fit(p@xtr,xtr_labels)
-y_hat = knn.predict(xte)
-acc = metrics.accuracy_score(y, y_hat)
+
+o_per_sets = np.arange(0.0, 0.6, 0.1)
+lam_sets = [2**i for i in range(3, -16, -1)]
+cv_fold_sets = range(optdata['cv_fold'])
+acc_all = []
+
+for i in data_sets:
+    optdata['dataset'] = i
+    for o in o_per_sets:
+        optdata['o_per'] = o
+        for l in lam_sets:
+            acc = 0
+            for c in cv_fold_sets:
+                # split the data, normalized and corrupted, in torch.tensor format
+                xtr, y_tr, xte, y_te = data_gen(data, labels, optdata)
+                Z, E = train_lrr(xtr, xtr, l, optdata)  # in the training it may use cuda
+                P = get_prj(xtr@Z)
+                knn.fit((P@xtr).t(), y_tr)
+                y_hat = knn.predict((P@xte).t())
+                acc = acc + metrics.accuracy_score(y_te, y_hat)
+            acc_all.append(acc/5)
+
+print(acc_all)
--- python/utils.py
+++ python/utils.py
@@ -29,19 +29,19 @@ def load_data(name='EYB'):
 
 def load_ar():
     data = pd.read_csv('AR55x40.csv', header=None).to_numpy()
-    labels = pd.read_csv('AR55x40_labels.csv', header=None).to_numpy()
+    labels = pd.read_csv('AR55x40_labels.csv', header=None).to_numpy().squeeze()
     return data, labels
 
 
 def load_eyb():
     data = pd.read_csv('EYB32x32.csv',header=None).to_numpy()
-    labels = pd.read_csv('EYB32x32_labels.csv',header=None).to_numpy()
+    labels = pd.read_csv('EYB32x32_labels.csv',header=None).to_numpy().squeeze()
     return data, labels
 
 
 def load_coil():
     data = pd.read_csv('coil20.csv',header=None).to_numpy()
-    labels = pd.read_csv('coil20_labels.csv',header=None).to_numpy()
+    labels = pd.read_csv('coil20_labels.csv',header=None).to_numpy().squeeze()
     return data, labels
 
 
@@ -56,7 +56,7 @@ def data_gen(data, labels, optdata):
     np.random.seed(optdata['rng']+100)
     ind_E = np.arange(MN)  # serialize the index
     np.random.shuffle(ind_E)  # shuffle index
-    data[ind_E[:oMN]] = np.random.randint(0, 255, oMN)  # randomly corrupt
+    data.ravel()[ind_E[:oMN]] = np.random.randint(0, 255, oMN)  # randomly corrupt
 
     data_norm = data / np.sqrt((data * data).sum(0))  # normalize per column
 
@@ -75,22 +75,22 @@ def data_gen(data, labels, optdata):
     xte_labels = np.array([])
 
     for i in range(C):
-        current_label = np.where(labels == i)
-        start_point = current_label[0]
-        end_point = current_label[-1]
+        current_label = np.where(labels == i+1) # current_label is a tuple
+        start_point = current_label[0][0]
+        end_point = current_label[0][-1]
 
         data_norm = data_norm.T
         np.random.seed(optdata['rng'])
         np.random.shuffle(data_norm[start_point:end_point, :])  # only shuffle the first dimension
         data_norm = data_norm.T
 
-        xtr = np.c_[xtr, data_norm[:, start_point:start_point+tr_len]]
-        xtr_labels =np.c_[xtr_labels, labels[start_point:start_point+tr_len]]
-        xte = np.c_[xte, data_norm[:, start_point+tr_len:end_point]]
-        xte_labels = np.c_[xte_labels, labels[start_point+tr_len:end_point]]
+        xtr = np.c_[xtr, data_norm[:, start_point:start_point+tr_len]] if xtr.size else data_norm[:, start_point:start_point+tr_len]
+        xtr_labels = np.r_[xtr_labels, labels[start_point:start_point+tr_len]] if xtr_labels.size else labels[start_point:start_point+tr_len]
+        xte = np.c_[xte, data_norm[:, start_point+tr_len:end_point]] if xte.size else data_norm[:, start_point+tr_len:end_point]
+        xte_labels = np.r_[xte_labels, labels[start_point+tr_len:end_point]] if xte_labels.size else labels[start_point+tr_len:end_point]
 
     # return xtr, xtr_labels, xte, xte_labels
-    return n2t(xtr), n2t(xte_labels), n2t(xte), n2t(xte_labels)
+    return n2t(xtr), n2t(xtr_labels), n2t(xte), n2t(xte_labels)
 
 
 def n2t(x):
@@ -99,31 +99,36 @@ def n2t(x):
 
 def train_lrr(X, A, lamb, optdata):
     # initialization
+    X = X.cuda() if optdata['use_gpu'] else X
+    A = A.cuda() if optdata['use_gpu'] else A
+
     m, n = X.shape
     mu = 0.1 * lamb
     atx = A.t()@X
-    inv_a = torch.inverse(A.t()@A + torch.eye(m))
+    inv_a = torch.inverse(A.t()@A + torch.eye(n))
 
-    J = torch.zeros(n, n).cuda if optdata['use_gpu'] else torch.zeros(n, n)
-    Z = torch.zeros(n, n).cuda if optdata['use_gpu'] else torch.zeros(n, n)
-    E = torch.zeros(m, n).cuda if optdata['use_gpu'] else torch.zeros(n, n)
-    Y1 = torch.zeros(m, n).cuda if optdata['use_gpu'] else torch.zeros(n, n)
-    Y2 = torch.zeros(n, n).cuda if optdata['use_gpu'] else torch.zeros(n, n)
+    J = torch.zeros(n, n).cuda() if optdata['use_gpu'] else torch.zeros(n, n)
+    Z = torch.zeros(n, n).cuda() if optdata['use_gpu'] else torch.zeros(n, n)
+    E = torch.zeros(m, n).cuda() if optdata['use_gpu'] else torch.zeros(m, n)
+    Y1 = torch.zeros(m, n).cuda() if optdata['use_gpu'] else torch.zeros(m, n)
+    Y2 = torch.zeros(n, n).cuda() if optdata['use_gpu'] else torch.zeros(n, n)
     diff = torch.zeros(optdata['max_iter'])
 
-    for iter in range(optdata['max_iter']):
+    # body of algorithm
+    for i in range(optdata['max_iter']):
         # update J
         temp = Z + Y2 / mu;
         [U, sigma, V] = torch.svd(temp)
         svp = sigma[sigma>1/mu].shape[0]
         if svp > 1:
             sigma = sigma[:svp] - 1/mu
+            J = U[:, :svp] @ sigma.diag() @ V[:, :svp].t()
         else:
             svp, sigma = 1, 0
-        J = U[:,:svp]@sigma.diag()*V[:,:svp].t()
+            J = U[:, :svp] *sigma @ V[:, :svp].t()
 
         # update Z
-        Z = inv_a * (atx - A.t()@E+J+(A.t()@Y1-Y2) / mu)
+        Z = inv_a @ (atx - A.t()@E+J+(A.t()@Y1-Y2) / mu)
 
         # update E
         xmaz = X - A @ Z
@@ -134,24 +139,26 @@ def train_lrr(X, A, lamb, optdata):
         leq1 = xmaz - E
         leq2 = Z - J
         stopC = np.maximum(leq1.abs().max(), leq2.abs().max())
-        diff[iter] = stopC;
-        if iter > 10 and (abs(diff(iter) - diff(iter - 10)) / abs(diff(iter)) < 1e-3)
+        diff[i] = stopC
+        if i > 10 and (abs(diff[i] - diff[i - 10]) / abs(diff[i]) < 1e-3):
             break
-        end
-        if stopC < optdata['tol'] :
+        if stopC < optdata['tol']:
             break
         else:
             Y1 = Y1 + mu * leq1
             Y2 = Y2 + mu * leq2
             mu = np.minimum(optdata['max_mu'], mu * optdata['rho'])
+    return Z, E
+
+
 
 def get_prj(x):
-    x0 = x-x.mean(1)
+    x0 = x- x.mean(1).reshape(x.shape[0], -1)
     u, s, _ = torch.svd(x0@x0.t())
     threshold = 1e4
     min_s = s.max()/threshold
     P = u[:, :s[s>min_s].shape[0]].t()
-    return P
+    return P.cpu()
 
 
 
