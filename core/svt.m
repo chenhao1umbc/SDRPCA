c37164142317220ab5fc5c42f01738684511807e
chenhao1UMBC <chenhao1@umbc.edu>
2019-08-08 16:47:45 -0400

    save all the results

--- core/svt.m
+++ core/svt.m
@@ -3,7 +3,11 @@ function [P] = svt(L, tau)
 % Var is initialed variables
 %opt is options
 % tau*norm(P, 'nuc')+ 0.5*norm(P-L, 'fro')
-[u, s, v] = svd(L);
+try
+    [u, s, v] = svd(L);
+catch
+    [u, s, v] = svd(L+ 1e-5*rand(size(L)));
+end
 s = s -tau;
 s(s<0) = 0;
 P = u*s*v';
