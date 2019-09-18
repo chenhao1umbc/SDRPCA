0d673b64130c9bda33aa275cac9d95fabad48079
chenhao1umbc <chenhao1@umbc.edu>
2019-09-18 11:25:10 -0400

    modify the code for GPU to run

--- main/sdrpca1.m
+++ main/sdrpca1.m
@@ -6,12 +6,14 @@ addpath(genpath('../../SDRPCA'))
 addpath(genpath('../../data_img'))
 
 % init data & and settings
+global optdata % to cooperate with existing code for GPU 
 try
-gpu(1)
-optdata.gpu = 1;
-fprintf('GPU is used \n')
+    gpuDevice(4);
+    optdata.gpu = 1;
+    fprintf('GPU is used \n')
 catch 
-fprintf('GPU is not available, calculating on cpu \n')
+    optdata.gpu = 0;
+    fprintf('GPU is not available, calculating on cpu \n')
 end
 optdata.ind_dataset = 1;% 1 is Extended Yale B, 0 is toy data
 optdata.add_outlier = true; % adding outlier or not
@@ -20,9 +22,42 @@ optdata.outlier_type = 'l1'; % l1 is l1 norm, l21 is l21 norm, no other options
 optdata.rng = 0; % random seed
 [X0,X0cv,X0test,T] = datgen(optdata); 
 [X,Xcv,Xtest,E] = out_norm(X0, X0cv, X0test, optdata);
-cv_fold = 5; % 3 folds cross-validation
 [Var0, opt] = initdata(X, optdata);
-opt.percentage
+cv_fold = 5; % 3 folds cross-validation
+nu_set = 2.^(-(-3:16));
+o_per_set = 0:0.1:0.5;
+acc = 0;
+%%
+tic
+for s = 1:3
+    optdata.ind_dataset = s;% 1 is Extended Yale B, 0 is toy data
+    acc_all = zeros(length(o_per_set), length(nu_set));
+    if optdata.gpu,  acc_all = gpu(zeros(length(o_per_set), length(nu_set))); end
+for o_per = 1:length(o_per_set)
+for n = 1:length(nu_set)
+
+for i = 1:cv_fold    
+optdata.o_per = o_per_set(o_per);% outlier percentage
+optdata.rng = i; % random seed
+
+% KNN classifier
+acc = acc + myknn(Xtr, X.label(1,:), Xtest, Proj); % k = 5
+end
+    acc_all(o_per, n) = acc/cv_fold
+    disp('dataset'); disp(s); 
+    acc = 0;
+end
+end    
+end
+toc
+
+
+
+
+
+
+
+
 for ii = 1:6
 optdata.o_per = 0.1*(ii -1);% outlier percentage
 optdata.outlier_type = 'l1'; % l1 is l1 norm, l21 is l21 norm, no other options
